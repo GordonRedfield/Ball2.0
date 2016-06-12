b20df798479a2c9d2964bc05d9219ebac788f2fc
huesihues <joanhuesoaguilar@gmail.com>
2016-06-12 23:21:07 +0200

    Muchos cambios

---
+++ Assets/Scripts/comp_camera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class comp_camera : MonoBehaviour 
+{
+
+public Transform ball;
+public Transform transformCamera;
+public float distanciaX;
+public float distanciaY;
+
+	// Use this for initialization
+	void Start () 
+	{
+	ball=GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	transformCamera=GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//	transformCamera.transform.position.x=ball.transform.position.x+10;
+//	transformCamera.transform.position.y=ball.transform.position.y;
+//	transformCamera.transform.position.z=ball.transform.position.z;
+		transform.LookAt(ball);
+		transformCamera.transform.position=new Vector3(ball.transform.position.x+distanciaX,ball.transform.position.y+distanciaY,ball.transform.position.z);
+
+	}
+}
